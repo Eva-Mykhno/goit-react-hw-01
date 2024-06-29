1c7e513cb29a6ac08be5b8ba97d59761d66e9357
Eva-Mykhno <154619097+Eva-Mykhno@users.noreply.github.com>
2024-06-29 19:34:59 +0300

    create.files.jsx

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,18 @@
+import s from "./FriendList.module.css";
+import FriendListItem from "../FriendListItem/FriendListItem";
+
+export const FriendList = ({ friends = [] }) => {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <li key={friend.id}>
+          <FriendListItem
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,11 @@
+import s from "./FriendListItem.module.css";
+
+export const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <div>
+      <img src={avatar} alt={name} width="48" />
+      <p>{name}</p>
+      <p>{isOnline}</p>
+    </div>
+  );
+};
